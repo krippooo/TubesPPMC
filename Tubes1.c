14124d488596f30391853ffc2efa1aabc78399bd
Andika Rama <116004310+krippooo@users.noreply.github.com>
2024-05-09 12:25:26 +0700

    Update Tubes1.c

--- Tubes1.c
+++ Tubes1.c
@@ -4,3 +4,38 @@
 #include <stdlib.h>
 #include <string.h>
 
+void djikstra_algorithm(){
+
+}
+
+void bfs(){
+
+}
+
+void dfs(){
+
+}
+
+void dynamic_prog(){
+
+}
+
+void a_star(){
+
+}
+
+void backtracking(){
+
+}
+
+void greedy(){
+
+}
+
+void divide_and_conquer(){
+
+}
+
+int main(){
+    
+}
