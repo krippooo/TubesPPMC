575906d01885477079ea39af3426445c22d24075
Andika Rama <116004310+krippooo@users.noreply.github.com>
2024-05-09 12:25:44 +0700

    Update Tubes2.c

--- Tubes2.c
+++ Tubes2.c
@@ -4,3 +4,46 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define rad 3,14159 / 180
+#define r 6371 //Kilometer
+
+
+void greedy(){
+
+}
+
+void brute_force(){
+
+}
+
+void bfs(){
+
+}
+
+void dfs(){
+
+}
+
+void branch_and_bound(){
+
+}
+
+void integer_linear_programming(){
+
+}
+
+void genetics_algorithm(){
+
+}
+
+void ant_colony_optimization(){
+
+}
+
+void particle_swarm_optimization(){
+
+}
+
+int main(){
+    
+}
